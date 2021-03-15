5cff7a47182940d1b10a650a123128197189ab31
Kanai Hiroki <kanai.hiroki12@gmail.com>
2021-03-16 00:31:23 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# kminimal
+
+a emacs minimal color theme.
+
+# Install
+
+Copy kminimal-theme.el to load-path (or custom-theme-load-path).
+
+```shell
+$ cp kminimal-theme.el <LOAD-PATH>
+```
+
+Then call `load-theme`.
+```elisp
+(load-theme 'kminimal t)
+```
+
+# Screenshots
+
+[![c](image/c.png)](image/c.png)
+[![python](image/python.png)](image/python.png)
+[![go](image/go.png)](image/go.png)
+
+# License
+
+public domain
---
+++ kminimal-theme.el
@@ -0,0 +1,43 @@
+;; public domain
+
+(deftheme kminimal
+  "a minimal color theme.")
+
+(custom-theme-set-faces
+ 'kminimal
+ '(default ((t (:foreground "white" :background "black"))))
+ '(cursor ((t (:inverse-video t))))
+ '(region ((t (:foreground "black" :background "white"))))
+
+ '(font-lock-builtin-face ((t (:foreground "white"))))
+ '(font-lock-constant-face ((t :foreground "#ff00ff")))
+ '(font-lock-preprocessor-face ((t (:inherit font-lock-constant-face))))
+ '(font-lock-keyword-face ((t (:foreground "#ffff00"))))
+ ;; '(font-lock-type-face ((t (:foreground "#ffff00")))) ;; TODO
+ '(font-lock-type-face ((t (:inherit font-lock-type-face)))) ;; TODO
+ '(font-lock-regexp-grouping-backslash ((t (:foreground "#ffff00"))))
+ '(font-lock-regexp-grouping-construct ((t (:foreground "#ffff00"))))
+ '(font-lock-variable-name-face ((t (:foreground "white"))))
+ '(font-lock-function-name-face ((t (:inherit font-lock-type-face))))
+ '(font-lock-string-face ((t (:inherit font-lock-constant-face))))
+ '(font-lock-comment-face ((t (:foreground "#00ffff"))))
+ '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
+ '(font-lock-doc-face ((t (:inherit font-lock-comment-face))))
+ '(font-lock-doc-string-face ((t (:inherit font-lock-comment-face))))
+ '(font-lock-warning-face ((t (:foreground "#cd0000")))) ;; TODO: underline?
+
+ '(trailing-whitespace ((t (:inherit font-lock-warning-face))))
+
+ '(link ((t (:foreground "#0000ee" :underline t))))
+ '(link-visited ((t (:foreground "#0000ee" :underline t))))
+ '(isearch ((t (:foreground "black" :background "#ffff00"))))
+ '(isearch-lazy-highlight-face ((t (:background "#ff00ff"))))
+ '(isearch-fail ((t (:background "#cd0000"))))
+ '(isearch-secondary ((t (:foreground "#cd0000"))))
+ '(query-replace ((t (:inherit (isearch)))))
+
+ '(next-error ((t (:background "#ffff00"))))
+ '(match ((t (:background "#ffff00"))))
+ )
+
+(provide-theme 'kminimal)
