b1189cee26fca9c000e099fa1f3c3088e51db33a
Kanai Hiroki <kanai.hiroki12@gmail.com>
2023-03-03 15:47:58 +0900

    color name

--- kminimal-theme.el
+++ kminimal-theme.el
@@ -5,45 +5,47 @@
 
 (custom-theme-set-faces
  'kminimal
- '(default ((t (:foreground "brightwhite" :background "black"))))
+ ;; '(default ((t (:foreground "brightwhite" :background "black"))))
+
  '(cursor ((t (:inverse-video t))))
- '(region ((t (:foreground "black" :background "brightwhite"))))
- '(bold-italic ((t (:foreground "brightwhite" :background "black" :underline t))))
+ ;; '(region ((t (:foreground "black" :background "brightwhite"))))
+ '(region ((t (:inverse-video t))))
+ '(bold-italic ((t (:underline t))))
 
- '(mode-line ((t (:foreground "black" :background "brightwhite"))))
- '(mode-line-inactive ((t (:inherit mode-line))))
- '(mode-line-buffer-id ((t (:foreground "black"))))
+ ;; '(mode-line ((t (:foreground "black" :background "brightwhite"))))
+ ;; '(mode-line-inactive ((t (:inherit mode-line))))
+ ;; '(mode-line-buffer-id ((t (:foreground "black"))))
  ;; '(shadow ((t (:foreground "blue" :background "black"))))
+ '(minibuffer-prompt ((t (:foreground "blue" :bold t))))
 
- '(font-lock-builtin-face ((t (:foreground "brightwhite"))))
- '(font-lock-constant-face ((t :foreground "#ff00ff")))
+ '(font-lock-builtin-face ((t (:foreground "yellow"))))
+ '(font-lock-constant-face ((t :foreground "magenta")))
  '(font-lock-preprocessor-face ((t (:inherit font-lock-constant-face))))
- '(font-lock-keyword-face ((t (:foreground "#ffff00"))))
- ;; '(font-lock-type-face ((t (:foreground "#ffff00")))) ;; TODO
- '(font-lock-type-face ((t (:inherit font-lock-type-face)))) ;; TODO
- '(font-lock-regexp-grouping-backslash ((t (:foreground "#ffff00"))))
- '(font-lock-regexp-grouping-construct ((t (:foreground "#ffff00"))))
- '(font-lock-variable-name-face ((t (:foreground "brightwhite"))))
+ '(font-lock-keyword-face ((t (:foreground "yellow"))))
+ '(font-lock-type-face ((t (:foreground "yellow"))))
+ '(font-lock-regexp-grouping-backslash ((t (:foreground "yellow"))))
+ '(font-lock-regexp-grouping-construct ((t (:foreground "yellow"))))
+ '(font-lock-variable-name-face ((t (:foreground "yellow"))))
  '(font-lock-function-name-face ((t (:inherit font-lock-type-face))))
  '(font-lock-string-face ((t (:inherit font-lock-constant-face))))
- '(font-lock-comment-face ((t (:foreground "#00ffff"))))
+ '(font-lock-comment-face ((t (:foreground "cyan"))))
  '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
  '(font-lock-doc-face ((t (:inherit font-lock-comment-face))))
  '(font-lock-doc-string-face ((t (:inherit font-lock-comment-face))))
- '(font-lock-warning-face ((t (:foreground "#cd0000")))) ;; TODO: underline?
+ '(font-lock-warning-face ((t (:foreground "red" :underline t))))
 
  '(trailing-whitespace ((t (:inherit font-lock-warning-face))))
 
- '(link ((t (:foreground "#0000ee" :underline t))))
- '(link-visited ((t (:foreground "#0000ee" :underline t))))
- '(isearch ((t (:foreground "black" :background "#ffff00"))))
- '(isearch-lazy-highlight-face ((t (:background "#ff00ff"))))
- '(isearch-fail ((t (:background "#cd0000"))))
- '(isearch-secondary ((t (:foreground "#cd0000"))))
+ '(link ((t (:foreground "cyan" :underline t))))
+ '(link-visited ((t (:foreground "cyan" :underline t))))
+ '(isearch ((t (:foreground "black" :background "yellow"))))
+ '(isearch-lazy-highlight-face ((t (:background "yellow"))))
+ '(isearch-fail ((t (:background "red"))))
+ '(isearch-secondary ((t (:foreground "red"))))
  '(query-replace ((t (:inherit (isearch)))))
 
- '(next-error ((t (:background "#ffff00"))))
- '(match ((t (:background "#ffff00"))))
+ '(next-error ((t (:background "yellow"))))
+ '(match ((t (:background "yellow"))))
  )
 
 (provide-theme 'kminimal)
